efeeaf875b09eba3a6378ee093cc10a56676ca9d
Vinícius Kappke <vinikappke@gmail.com>
2022-08-11 16:24:47 -0300

    script to fix java compiling erros on windows

---
+++ KILL JAVA ELIMINATE IT DESTROY IT.bat
@@ -0,0 +1,16 @@
+@echo off
+echo omfg i hate java so much why do i still do this shit im gonna kms :D
+echo.
+echo.
+echo killing java bc it deserves the pain
+echo.
+taskkill /f /im jqs.exe
+taskkill /f /im javaw.exe
+taskkill /f /im java.exe
+echo.
+echo now that java is dead im gonna kill vscode and ressurrect him
+echo.
+taskkill /f /im Code.exe
+code
+echo.
+echo done!
