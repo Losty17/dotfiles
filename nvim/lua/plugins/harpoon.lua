286a3ad245e3b5abb26904d446b769f1c8146704
Vinícius Kappke <vinikappke@gmail.com>
2024-12-24 13:46:36 -0300

    fix harpoon shortcuts

--- nvim/lua/plugins/harpoon.lua
+++ nvim/lua/plugins/harpoon.lua
@@ -13,13 +13,13 @@ return {
     -- vim.keymap.set("n", "<C-e>", function() harpoon.ui:toggle_quick_menu(harpoon:list()) end)
 
     vim.keymap.set("n", "<C-h>", function() harpoon:list():select(1) end)
-    vim.keymap.set("n", "<C-t>", function() harpoon:list():select(2) end)
-    vim.keymap.set("n", "<C-n>", function() harpoon:list():select(3) end)
-    vim.keymap.set("n", "<C-s>", function() harpoon:list():select(4) end)
+    vim.keymap.set("n", "<C-j>", function() harpoon:list():select(2) end)
+    vim.keymap.set("n", "<C-k>", function() harpoon:list():select(3) end)
+    vim.keymap.set("n", "<C-l>", function() harpoon:list():select(4) end)
 
     -- Toggle previous & next buffers stored within Harpoon list
-    vim.keymap.set("n", "<C-S-P>", function() harpoon:list():prev() end)
-    vim.keymap.set("n", "<C-S-N>", function() harpoon:list():next() end)
+    -- vim.keymap.set("n", "<C-S-P>", function() harpoon:list():prev() end)
+    -- vim.keymap.set("n", "<C-S-N>", function() harpoon:list():next() end)
 
     -- basic telescope configuration
     local conf = require("telescope.config").values
